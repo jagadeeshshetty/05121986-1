109aa9b7849ade13b54568148a292f70e94b50f2
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2022-09-04 13:16:22 +0530

    updated the welcome message.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`Made with 💖 form 🇮🇳 - Jagadeesh C`));
 
 app.get('/live', (req, res) => res.status(200).send('OK'));
 
