bea8b188813650f0572570a09a641b3bbb14d6a0
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2022-09-04 13:04:15 +0530

    update

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/live', (req, res) => {
-  res.status(200).send('OK');
-});
+app.get('/live', (req, res) => res.status(200).send('OK'));
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
